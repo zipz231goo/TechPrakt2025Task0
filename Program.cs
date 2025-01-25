1d2b84aa0bcd604e5c1d6262d61246665355d38a
Hiruk <zipz231_goo@student.ztu.edu.ua>
2025-01-25 20:03:22 +0200

    ad gitignore file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо користувача про перше число
+        Console.Write("Введіть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        // Запитуємо користувача про друге число
+        Console.Write("Введіть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Обчислюємо суму
+        double sum = num1 + num2;
+
+        // Виводимо результат
+        Console.WriteLine($"Сума двох чисел: {sum}");
+    }
+}
